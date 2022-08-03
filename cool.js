d93487c2e9cb8554733046748daa84d4b1fb6f7b
Vương Minh Khánh <109456581+VuongMinhKhanh@users.noreply.github.com>
2022-08-03 16:44:16 +0700

    wakarimasu

--- cool.js
+++ cool.js
@@ -1,2 +1,21 @@
 //Scroll down to sponsors h3 => activate animation
+$(document).ready(function(){
+    window.addEventListener('scroll', reveal);
+    function reveal() 
+    {
+        var revealElement = document.querySelectorAll('.reveal');
+        for (let i = 0; i < revealElement.length; i++)
+        {
+            let windowHeight = window.innerHeight;
+            let revealTop = revealElement[i].getBoundingClientRect().top;
+            let revealPoint = 150;
+            if (revealTop < windowHeight - revealPoint)
+                revealElement[i].classList.add('active');
+
+            else 
+                revealElement[i].classList.remove('active'); 
+        }
+    }
+}); 
+
 // automatically adjust clip-path's percentage
