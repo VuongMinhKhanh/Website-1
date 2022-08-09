8d8993cb7e175a9dee325fb0dc49b6e9b5cc54ac
Vương Minh Khánh <109456581+VuongMinhKhanh@users.noreply.github.com>
2022-08-10 01:56:03 +0700

    hello mấy cưng

--- cool.js
+++ cool.js
@@ -1,4 +1,4 @@
-//Scroll down to sponsors h3 => activate animation
+//Scroll down to elements => activate css
 $(document).ready(function(){
     window.addEventListener('scroll', reveal);
     function reveal() 
@@ -11,11 +11,29 @@ $(document).ready(function(){
             let revealPoint = 150;
             if (revealTop < windowHeight - revealPoint)
                 revealElement[i].classList.add('active');
-
             else 
                 revealElement[i].classList.remove('active'); 
         }
     }
+    $(".burger-icon").click(function() {
+        $(".navMobile").toggleClass("mobile");
+    });
+    $(".burger-icon").click(function() {
+        $(".topBun").toggleClass("topBunRotate").toggleClass("afterTop");
+    });
+    $(".burger-icon").click(function() {
+        $(".patty").toggleClass("pattyRotate").toggleClass("afterPatty");
+    });
+    $(".burger-icon").click(function() {
+        $(".bottomBun").toggleClass("bottomBunRotate").toggleClass("afterBottom");
+    });
+    $(".products > a").click(function() {
+        $(".product").toggleClass("open");
+    //    if ($(".navMobile.product").css('display') === 'none' ) $(".navMobile.product").css('display','block !important');
+    //    else $(".navMobile.product").css('display','none !important');
+    });
+    
 }); 
+
 
 // automatically adjust clip-path's percentage
